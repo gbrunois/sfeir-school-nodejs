35066b1f845b52233052944ebeb24a9406816100
GBrunois <brunois.g@sfeir.com>
2019-10-15 22:08:44 +0200

    :gift: demo child process

---
+++ demo/child_process/child_process.spawn.js
@@ -0,0 +1,15 @@
+const { spawn } = require('child_process');
+const child = spawn('pwd');
+
+child.on('exit', function (code, signal) {
+    console.log('child process exited with ' +
+                `code ${code} and signal ${signal}`);
+  });
+
+  child.stdout.on('data', (data) => {
+    console.log(`child stdout:\n${data}`);
+  });
+  
+  child.stderr.on('data', (data) => {
+    console.error(`child stderr:\n${data}`);
+  });
---
+++ demo/child_process/child_process.spawn_pipe.js
@@ -0,0 +1,10 @@
+//use CTRL+D to send EOF
+const { spawn } = require('child_process');
+
+const child = spawn('wc');
+
+process.stdin.pipe(child.stdin)
+
+child.stdout.on('data', (data) => {
+  console.log(`child stdout:\n${data}`);
+});
